37ac5aaf512984ad15238a20c4d60a334c5f37ed
krzysiekstefanski <krzysiekstefanski@gmail.com>
2024-04-01 02:56:30 +0200

    Main banner update for 8 edition

--- src/atomic/templates/Banner/Banner.jsx
+++ src/atomic/templates/Banner/Banner.jsx
@@ -86,11 +86,8 @@ const Banner = ({ theme }) => (
         position="relative"
         zIndex="1"
       >
-        {theme === "niepodleglosci" ? (
+        {theme === "niepodleglosci" && (
           <StaticImage src="../../../images/bieg-niepodleglosci.jpg" />
-        ) : (
-          // <StaticImage src="../../../images/logobaner_transgr.2023.jpg" />
-          <StaticImage src="../../../images/7.gtfb._00020.jpg" />
         )}
         {/* <Box column width="100%" max="664px" margin="32px 0 24px"> */}
         <Box column width="100%" max="756px" margin="32px 0 24px">
@@ -134,50 +131,20 @@ const Banner = ({ theme }) => (
                 margin="0 0 8px"
                 marginMD="0 0 16px"
               >
-                W 7 edycji Gryfińskiego Transgranicznego Festiwalu Biegowego wzięło udział łacznie 237 zawodników.
+                31 marca zakończy się 1 termin zapisów na nasz Festiwal. Jednak realnie, jeżeli zarejestrowany uczestnik nie opłacił startowego do piątku 29 marca to nie ma szansy na zaksięgowanie opłaty na koncie organizatora (Święta). Wchodzimy więc w II termin a to wiąże się wyższymi kosztami i mamy nadzieję, że 10 zł więcej i będzie stanowić dla Was przeszkody do rejestracji (regulamin).
               </Text>
-              <Text
-                size="contentM"
-                margin="0 0 8px"
-                marginMD="0 0 16px"
-              >
-                Pogoda dopisała i nic nie stało na przeszkodzie aby uzyskiwać dobre rezultaty i bylo to widoczne jak zawodnicy walczyli o lepszy czas do samej mety.
+              <Text>
+                Dla zachęty prezentujemy świeże projekty ładnego medalu i koszulki. Zastrzegamy sobie że w produkcie końcowym mogą być drobne różnice.
               </Text>
-              <Text
-                size="contentM"
-                margin="0 0 8px"
-                marginMD="0 0 16px"
-              >
-                W biegu na 10 km zwyciężył Paweł Turbak z Sulęcina pokonując trasę w 34:58 s. Jako drugi na mecie zameldował się Andrzej Partika ze Szczecina (36:24 s), który wyprzedził trzeciego na mecie Jarosława Żabickiego o  blisko 30 s. Wśród kobiet triumfowała gryfinianka Julia Sanecka, która na pokonanie trasy potrzebowała 39 min. i 46 sekund. Jako druga mineła mętę Justyna Hałka z Łobza (43:24 s) a jako trzecia Klaudia Kobydecka (Kostrzyń nad Odrą) w czasie 44:42 s.
-              </Text>
-              <Text
-                size="contentM"
-                margin="0 0 8px"
-                marginMD="0 0 16px"
-              >
-                W biegu na 21,097 km (półmaraton) na pierwszym miejscu wbiegł Andrzej Pańko z Żarczyna z bardzo dobrym rezultatem - 1:14:52 s. Kolejni zawodnicy wbiegali już ze sporą stratą bo Bartosz Przekopowicz uzyskał wynik 1:19:06 s i kolejnego na mecie Jacka Rudzińskiego z Gryfina wyprzedził o 7 s. Najlepszą wśród kobiet okazała się Beata Białek (także z Żarczyna!) pokonując półmaraton w czasie 1:26:59 s. wyprzedzając drugą zawodniczkę Iwonę Drozdek z Kobylanki o ponad 5 minut (1:32:14). Jako trzecia na metę melduje się Klaudia Czajczyńska ze Szczecina przegrywając drugie miejsce o równą minutę.
-              </Text>
-              <Text
-                size="contentM"
-                margin="0 0 8px"
-                marginMD="0 0 16px"
-              >
-                W rywalizacji w Nordic Walking na 10 km zwyciężył Marcin Wesołowski ze Szczecina przed Pawłem Chojnackim z Mierzyna i Tomaszem Strzelcem z Gryfina. W klasyfikacji kobiet zwyciężyła Marzena Olejnik ze Szczecina przed Anną Stasiowską z Gryfina i Agnieszką Górską ze Szczecina.
-              </Text>
-              <CustomLink href="https://domtel-sport.pl/uploads/wyniki/14947.pdf" target="_blank"
-                rel="noopener noreferrer" margin="0 0 4px">Komunikat z biegu na 10 km.</CustomLink>
-              <CustomLink href="https://domtel-sport.pl/uploads/wyniki/14949.pdf" target="_blank"
-                rel="noopener noreferrer" margin="0 0 4px">Komunikat z półmaratonu.</CustomLink>
-              <CustomLink href="https://domtel-sport.pl/uploads/wyniki/14948.pdf" target="_blank"
-                rel="noopener noreferrer" margin="0 0 8px">Komunikat z Nordic Walking na 10 km.</CustomLink>
             </>
           )}
         </Box>
         {/* <Button as={Link} to={`/bieg${theme}/zapisy`} variant={theme}> */}
         {theme === "transgraniczny" && (
-          <Button as={Link} to={`#galeria`} variant={theme}>
-            Zobacz zdjęcia
-          </Button>
+          <>  
+            <StaticImage src="../../../images/05f3bffe-f0d1-4cbb-b294-8c84c1499cb3.jpg" />
+            <StaticImage src="../../../images/0a891bfe-c147-44bb-bf09-586d1ef65886.jpg" />
+          </>
         )}
       </Box>
     </Container>
