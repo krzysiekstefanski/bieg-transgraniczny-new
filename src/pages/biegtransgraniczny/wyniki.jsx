7a458496a755e634a9b3168f4ab4546071436766
krzysiekstefanski <krzysiekstefanski@gmail.com>
2024-05-04 08:39:07 +0200

    Update wyniki.jsx

--- src/pages/biegtransgraniczny/wyniki.jsx
+++ src/pages/biegtransgraniczny/wyniki.jsx
@@ -8,31 +8,6 @@ import Container from "../../atomic/molecules/Container/Container"
 
 const Wrapper = styled(Box)`
   width: 100%;
-  height: 3930px;
-
-  @media (min-width: 445px) {
-    height: 4150px;
-  }
-
-  @media (min-width: 576px) {
-    height: 4200px;
-  }
-
-  @media (min-width: 593px) {
-    height: 3800px;
-  }
-
-  @media (min-width: 785px) {
-    height: 2700px;
-  }
-
-  @media (min-width: 1009px) {
-    height: 2200px;
-  }
-
-  @media (min-width: 1217px) {
-    height: 2023px;
-  }
 `
 
 const TransgranicznyWynikiPage = () => {
