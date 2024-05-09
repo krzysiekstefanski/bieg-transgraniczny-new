3898c09920d127053269179490379583a527b4f0
krzysiekstefanski <krzysiekstefanski@gmail.com>
2024-05-09 20:14:29 +0200

    API test

--- src/pages/biegniepodleglosci/lista-startowa.jsx
+++ src/pages/biegniepodleglosci/lista-startowa.jsx
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 import Box from "../../atomic/atoms/Box/Box"
 
 const NiepodleglosciListaStartowaPage = () => {
   const theme = "niepodleglosci"
+  const [test, setTest] = useState([])
+  useEffect(() => {
+    fetch(api)
+      .then(response => response.json())
+      .then(resultData => {
+        setTest(resultData)
+      })
+  }, [])
+
+  useEffect(() => {
+    console.log(test)
+  }, [test])
 
   return (
     <Layout theme={theme}>
@@ -18,6 +30,13 @@ const NiepodleglosciListaStartowaPage = () => {
           style={{ border: "none" }}
         ></iframe>
       </Box>
+      {/* <Box column>
+        {test.map((item, index) => (
+          <Box
+            key={index}
+          >{`${item.playerNumber} ${item.firstname} ${item.lastname} ${item.clubname} ${item.pay}`}</Box>
+        ))}
+      </Box> */}
     </Layout>
   )
 }
