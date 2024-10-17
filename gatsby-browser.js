2cb0251c5f5953b83f422d97710c02b0d9732784
krzysiekstefanski <krzysiekstefanski@gmail.com>
2024-10-17 05:11:42 +0200

    Update gatsby-browser.js

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { GlobalStyle } from "./src/components-gb/single/globalStyle/globalStyle.const"
-
-export const wrapPageElement = ({ element }) => {
-  ;<>
-    <GlobalStyle />
-    {element}
-  </>
-}
