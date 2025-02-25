9b1f61a7a4658bc46f1e00d20e5e2887a210b3ff
krzysiekstefanski <krzysiekstefanski@gmail.com>
2025-02-26 01:36:46 +0800

    Update index.tsx

--- src/pages/biegtransgraniczny/zapisy/index.tsx
+++ src/pages/biegtransgraniczny/zapisy/index.tsx
@@ -1,9 +1,60 @@
 import React from "react"
 import styled from "styled-components"
-import { Layout, Seo, Container, Register } from "../../../components-gb"
+import { Layout, Seo, Container } from "../../../components-gb"
 import { graphql } from "gatsby"
 import { Color, EventTheme } from "../../../enums-gb"
 import { StyledComponent } from "../../../interfaces"
+
+const Wrapper: StyledComponent<"div"> = styled.div`
+  display: flex;
+  height: 3930px;
+  width: 100%;
+  margin-bottom: 24px;
+
+  @media (min-width: 400px) {
+    height: 4010px;
+  }
+
+  @media (min-width: 446px) {
+    height: 4100px;
+  }
+
+  @media (min-width: 500px) {
+    height: 4140px;
+  }
+
+  @media (min-width: 525px) {
+    height: 4160px;
+  }
+
+  @media (min-width: 550px) {
+    height: 4180px;
+  }
+
+  @media (min-width: 576px) {
+    height: 3800px;
+  }
+
+  @media (min-width: 654px) {
+    height: 3750px;
+  }
+
+  @media (min-width: 768px) {
+    height: 2650px;
+  }
+
+  @media (min-width: 992px) {
+    height: 2200px;
+  }
+
+  @media (min-width: 1152px) {
+    height: 2100px;
+  }
+
+  @media (min-width: 1200px) {
+    height: 2060px;
+  }
+`
 
 const Overlay: StyledComponent<"div"> = styled.div`
   min-height: calc(100vh - 285px);
@@ -16,23 +67,21 @@ const ZapisyTransgranicznyPage: React.FC = ({ data }): JSX.Element => {
   const { pageCore, logo, top, banner, verification, gallery } =
     data.transgranicznyPage
   const theme = EventTheme.Transgraniczny
-  const api = 13665
 
   return (
     <Layout data={{ pageCore, partners, logo }}>
       <Overlay>
         <Container>
-          {/* <Wrapper>
-          <iframe
-            id="zapisy-ramka"
-            title="Zapisy na bieg"
-            width="100%"
-            height="100%"
-            src="https://zapisy.domtel-sport.pl/9-gryfinski-transgraniczny-festiwal-biegowy-v13665"
-            style={{ border: "none" }}
-          ></iframe>
-        </Wrapper> */}
-          <Register api={api} />
+          <Wrapper>
+            <iframe
+              id="zapisy-ramka"
+              title="Zapisy na bieg"
+              width="100%"
+              height="100%"
+              src="https://zapisy.domtel-sport.pl/9-gryfinski-transgraniczny-festiwal-biegowy-v13665"
+              style={{ border: "none" }}
+            ></iframe>
+          </Wrapper>
         </Container>
       </Overlay>
     </Layout>
